06a33b667e2ec4f9a3c40653d9710ea6877a4afc
bigvvill <wcurtisusa@gmail.com>
2022-10-15 12:04:36 -0400

    Add project files.

---
+++ FlashCardApp/Models/Card.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlashCardApp.Models
+{
+    internal class Card
+    {
+        public int Id { get; set; }
+        public string CardFront { get; set; }
+        public string CardBack { get; set; }
+        public int StackId { get; set; }
+    }
+}
---
+++ FlashCardApp/Models/CardStack.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlashCardApp.Models
+{
+    internal class CardStack
+    {
+        public int CardStackID { get; set; }
+        public string CardStackName { get; set; }
+    }
+}
